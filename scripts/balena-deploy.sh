e4c867e7ad9e48645e6d7705db447acb2cf52d9a
Tomás Migone <tomasmigone@gmail.com>
2019-06-09 19:28:09 -0300

    Add deployment script to deal with .gitignore on balena

--- scripts/balena-deploy.sh
+++ scripts/balena-deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-mv homeassistant/.gitignore homeassistant/.gitignore.dont-ignore-me
-mv homeassistant/.gitignore-balena homeassistant/.gitignore
+rm homeassistant/.gitignore
+cp homeassistant/.gitignores/.gitignore-balena homeassistant/.gitignore
 
 git push balena master
 
-mv homeassistant/.gitignore homeassistant/.gitignore-balena
-mv homeassistant/.gitignore.dont-ignore-me homeassistant/.gitignore
+rm homeassistant/.gitignore
+cp homeassistant/.gitignores/.gitignore-full homeassistant/.gitignore
